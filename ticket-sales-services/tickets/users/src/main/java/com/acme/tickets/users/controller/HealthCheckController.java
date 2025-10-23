75dc05cc14239fa5a43adb1065c56ca80853697b
KassioRF <kassio_rf@hotmail.com>
2025-10-23 13:13:10 -0300

    feat(user.healthcheck): added a base route 'GET /' with a simple app health check

---
+++ ticket-sales-services/tickets/users/src/main/java/com/acme/tickets/users/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.acme.tickets.users.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        return ResponseEntity.ok(
+                Map.of("message", "User Service is up and running teste teste"));
+    }
+}
