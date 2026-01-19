57496194828cfb885f854e921a270df6e153b257
KassioRF <kassio_rf@hotmail.com>
2026-01-19 16:42:17 -0300

    feat(sales-app): Added FeignClients support

--- ticket-sales-services/tickets/sales/src/main/java/com/acme/tickets/sales/SalesApplication.java
+++ ticket-sales-services/tickets/sales/src/main/java/com/acme/tickets/sales/SalesApplication.java
@@ -2,12 +2,14 @@ package com.acme.tickets.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SalesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
 
 }
