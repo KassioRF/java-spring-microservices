a45fc3a66a8f8490d6293149e00b6e96e3331fd4
KassioRF <kassio_rf@hotmail.com>
2026-02-24 17:53:32 -0300

    feat(sales.routes): added findById route

--- ticket-sales-services/tickets/sales/src/main/java/com/acme/tickets/sales/controller/SaleController.java
+++ ticket-sales-services/tickets/sales/src/main/java/com/acme/tickets/sales/controller/SaleController.java
@@ -1,10 +1,12 @@
 package com.acme.tickets.sales.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,12 @@ public class SaleController {
     @PostMapping
     public ResponseEntity<SaleDTO> create(@RequestBody CreateSaleDTO payloadDTO) {
         return ResponseEntity.ok(service.create(payloadDTO));
+
+    }
+
+    @GetMapping("id/{id}")
+    public ResponseEntity<SaleDTO> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getById(id));
     }
 
 }
