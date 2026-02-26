10f94f26fa98999fef96da6538bc7082ca5923ab
KassioRF <kassio_rf@hotmail.com>
2026-02-25 23:20:22 -0300

    feat(webapp): initialize web app

---
+++ ticket-sales-services/web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex justify-center items-center font-bold">
+        <h1 className="text-5xl"> Hello Tickes App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
